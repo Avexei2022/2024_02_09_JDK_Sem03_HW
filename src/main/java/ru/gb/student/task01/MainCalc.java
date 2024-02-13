6840594e08a1a3bb616531a9b2e6a34268052e4e
alex123 <alex123@dot.com>
2024-02-13 09:20:12 +0500

    Seminar 3 HW fixed

--- src/main/java/ru/gb/student/task01/MainCalc.java
+++ src/main/java/ru/gb/student/task01/MainCalc.java
@@ -9,32 +9,23 @@ public class MainCalc {
         double doubleNum = 5.5555555555;
         byte byteNum = -127;
         short shortNum = 22222;
-        String string = "www";
         printResult(intNum, longNum, "+");
         printResult(longNum, floatNum, "*");
         printResult(floatNum, doubleNum, "/");
         printResult(doubleNum, intNum, "-");
         printResult(byteNum, shortNum, "/");
-;
     }
 
-    private static <Object extends Number> void printResult(Object o1, Object o2, String operand) {
-        String a = o1.toString();
-        String b = o2.toString();
-        String c = "";
-        switch (operand) {
-            case "+":
-                c = Double.toString(Calculator.sum(o1, o2));
-                break;
-            case "*":
-                c = Double.toString(Calculator.multiply(o1, o2));
-                break;
-            case "/":
-                c = Double.toString(Calculator.divide(o1, o2));
-                break;
-            case "-":
-                c = Double.toString(Calculator.subtract(o1, o2));
-        }
+    private static <T extends Number> void printResult(T t1, T t2, String operand) {
+        String a = t1.toString();
+        String b = t2.toString();
+        String c = switch (operand) {
+            case "+" -> Double.toString(Calculator.sum(t1, t2));
+            case "*" -> Double.toString(Calculator.multiply(t1, t2));
+            case "/" -> Double.toString(Calculator.divide(t1, t2));
+            case "-" -> Double.toString(Calculator.subtract(t1, t2));
+            default -> "";
+        };
         System.out.printf("\n%s %s %s = %s\n", a, operand, b, c);
     }
 
